904f8162c77a4f88b1f1d1027eaa6380e8695435
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-07-19 17:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  let array =[];
+  for (let index = 0, l = musicians.length; index < l; index++){
+    
+     array.push(musicians[index] + " plays " + instruments[index]);
+  }
+  return array
+}
+
+
+
+
+
+function johnLennonFacts(facts){
+  let array = [];
+    let i = 0;
+    while (i < 4) {
+        
+        array.push(array[i] + " !!! ");
+        return array
+ }
+
+}
+
+function iLoveTheBeatles(number){
+  let array = [];
+  let text = "I love the beatles"
+  let i = 0
+  do {
+    array.push[i](text += i; i++;);
+    
+  }
+  while(i < 15);
+    
+  }
+  return array
+  
+}
+
+
+ 
+
+  
+  
